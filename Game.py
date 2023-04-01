3fbf9f4299a034ed7d5f501595abcf09a7c3d385
AntonioMexD <toomedinaamp2010@gmail.com>
2023-03-31 23:02:22 -0400

    Added logic player and board

--- Game.py
+++ Game.py
@@ -1,1 +1,7 @@
 from teeko import Teeko
+from teeko import HumanPlayer
+from teeko import RandomPlayer
+game = Teeko()
+player1 = HumanPlayer()
+player2 = RandomPlayer()
+game.play_game(player1, player2)
--- teeko.py
+++ teeko.py
@@ -1,6 +1,7 @@
 import copy
 import numpy as np
 import math
+import random
 
 Board_Size = 4
 
@@ -103,3 +104,108 @@ class Teeko:
                         if beta <= alpha:
                             break
             return min_val
+        
+    def evaluate(self):
+        return self.minimax_alphabeta(3, -np.inf, np.inf)
+    
+    def choose_move(self, depth):
+        best_move = None
+        if self.current_player == 1:
+            best_val = -np.inf
+            for action in self.actions():
+                self.result(action)
+                value = self.minimax_alphabeta(depth, -np.inf, np.inf)
+                self.board[action] = 0
+                self.current_player = 1
+                if value > best_val:
+                    best_val = value
+                    best_move = action
+        else:
+            best_val = np.inf
+            for action in self.actions():
+                self.result(action)
+                value = self.minimax_alphabeta(depth, -np.inf, np.inf)
+                self.board[action] = 0
+                self.current_player = 2
+                if value < best_val:
+                    best_val = value
+                    best_move = action
+        return best_move
+        
+    def play_game(self, player1, player2, print_board=True):
+        """
+        Play a game of Teeko between two players.
+        """
+        while not self.is_terminal():
+            if self.current_player == 1:
+                action = player1.get_action(self)
+            else:
+                action = player2.get_action(self)
+            self.result(action)
+            if print_board:
+                print(self.board)
+                print()
+        if self.has_won(1):
+            print("Player 1 wins!")
+        elif self.has_won(2):
+            print("Player 2 wins!")
+        else:
+            print("It's a tie!")
+
+
+class HumanPlayer:
+    """
+    A human player for Teeko. Asks for user input to get the next action.
+    """
+    def get_action(self, game):
+        print("Current board:")
+        print(game.board)
+        while True:
+            move = input("Enter your move as 'row,col': ")
+            move = move.split(",")
+            try:
+                move = tuple([int(x) for x in move])
+                if move not in game.actions():
+                    print("Invalid move.")
+                else:
+                    return move
+            except ValueError:
+                print("Invalid move.")
+
+                
+class RandomPlayer:
+    """
+    A player that chooses a random valid action.
+    """
+    def get_action(self, game):
+        return random.choice(game.actions())
+
+
+class MiniMaxPlayer:
+    """
+    A player that uses minimax with alpha-beta pruning to choose actions.
+    """
+    def __init__(self, depth):
+        self.depth = depth
+
+    def get_action(self, game):
+        best_move = None
+        if game.current_player == 1:
+            best_score = -np.inf
+            for move in game.actions():
+                game_copy = copy.deepcopy(game)
+                game_copy.result(move)
+                score = game_copy.minimax_alphabeta(self.depth, -np.inf, np.inf)
+                if score > best_score:
+                    best_score = score
+                    best_move = move
+        else:
+            best_score = np.inf
+            for move in game.actions():
+                game_copy = copy.deepcopy(game)
+                game_copy.result(move)
+                score = game_copy.minimax_alphabeta(self.depth, -np.inf, np.inf)
+                if score < best_score:
+                    best_score = score
+                    best_move = move
+        return best_move
